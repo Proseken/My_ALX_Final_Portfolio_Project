5294a8d967c6f5d2088d54cb5e65d021e7a43de7
Proseken <sunnydaynduke1@gmail.com>
2025-01-08 05:17:49 +0100

    forms added

---
+++ forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, TextAreaField, RadioField, FileField, BooleanField
+from wtforms.validators import InputRequired, Email, Optional, NumberRange
+
+# Flask-WTF form
+class NotaryRequestForm(FlaskForm):
+    fullName = StringField('Full Name', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    age = IntegerField('Age (optional)', validators=[Optional(), NumberRange(min=10, max=99)])
+    service_type = SelectField(
+        'Service Type',
+        choices=[
+            ('document_notarization', 'Document Notarization'),
+            ('affidavit', 'Affidavit Preparation'),
+            ('certified_copy', 'Certified True Copy'),
+            ('oaths', 'Oaths and Declarations'),
+            ('wills', 'Wills and Testaments'),
+            ('contract', 'Contract Notarization'),
+            ('other', 'Other')
+        ],
+        validators=[InputRequired()]
+    )
+    document = FileField('Upload Document', validators=[Optional()])
+    terms = BooleanField('I agree to the terms and conditions', validators=[InputRequired()])
+    user_category = RadioField(
+        'This document is for',
+        choices=[
+            ('Minor', 'Minor'),
+            ('Adult', 'Adult (Parent/Guardian)'),
+            ('Acquaintance', 'Relative or Friend')
+        ],
+        validators=[InputRequired()]
+    )
+    comment = TextAreaField('Any comments or suggestions?', validators=[Optional()])
